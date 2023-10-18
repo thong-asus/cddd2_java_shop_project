3096120e1ec41222a04160d5ffd91925cda37149
VuAnhTDC <21211tt1568@mail.tdc.edu.vn>
2023-10-18 16:21:02 +0700

    màn hình home của cửa hàng và màn hình danh sách sản phẩm của cửa hàng

---
+++ app/src/main/java/com/example/duancuahang/HomeShop.java
@@ -0,0 +1,55 @@
+package com.example.duancuahang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class HomeShop extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    LinearLayout linearLayout_SanPhamCuaToi_ScreenHome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_shop);
+        setControl();
+        setEvent();
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+//    Xu ly su kien
+    private void setEvent() {
+        linearLayout_SanPhamCuaToi_ScreenHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+//    Anh xa
+    private void setControl() {
+        toolbar = findViewById(R.id.toolBar_ScreenHome);
+        linearLayout_SanPhamCuaToi_ScreenHome = findViewById(R.id.linearLayout_SanPhamCuaToi_ScreenHome);
+    }
+
+//    Thêm item và ActionBar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+}
