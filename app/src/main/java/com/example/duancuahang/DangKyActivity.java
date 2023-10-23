651840f6af70c5c9d6c64cef112a2c7b4755aedb
thong-asus <truongthong.asustuf@gmail.com>
2023-10-23 12:53:17 +0700

    Quên mật khẩu done

--- app/src/main/java/com/example/duancuahang/DangKyActivity.java
+++ app/src/main/java/com/example/duancuahang/DangKyActivity.java
@@ -2,8 +2,11 @@ package com.example.duancuahang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -13,21 +16,35 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.duancuahang.Class.Validates;
+
 public class DangKyActivity extends AppCompatActivity {
     EditText edtSoDienThoai, edtTenNguoiDKBanHang,edtTenCuaHang,edtDiaChiCuaHang,edtEmailCuaHang,edtMaSoThue;
     ImageView imgCCCDFront, imgCCCDBack;
     CheckBox chkDongYDieuKhoan;
-    Button btn_tiepTuc;
+    Button btnTiepTuc;
     TextView tvDaCoTaiKhoan;
+    private Context context;
+    private static final int PICK_IMAGE_FRONT = 2;
+    private static final int PICK_IMAGE_BACK = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ky);
         setControl();
         setEvent();
+        context = this;
     }
 
     private void setEvent() {
+        btnTiepTuc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Nếu tất cả các thông tin nhập đầy đủ và checkbox sẽ chuyển sang màn hình nhập otp xác minh tài khoản
+                Intent intent = new Intent(context, InputOTPSignUpActivity.class);
+                context.startActivity(intent);
+            }
+        });
         edtSoDienThoai.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
@@ -61,7 +78,7 @@ public class DangKyActivity extends AppCompatActivity {
                     if(Validates.validShopName(edtTenCuaHang.getText().toString()) == false) {
                         edtTenCuaHang.selectAll();
                         edtTenCuaHang.requestFocus();
-                        edtTenCuaHang.setError("Tên cửa hàng từ 8-100 ký tự chữ số");
+                        edtTenCuaHang.setError("Tên cửa hàng từ 8-100 ký tự chữ và số");
                     }
                 }
                 return false;
@@ -74,7 +91,7 @@ public class DangKyActivity extends AppCompatActivity {
                     if(Validates.validShopAddress(edtDiaChiCuaHang.getText().toString()) == false) {
                         edtDiaChiCuaHang.selectAll();
                         edtDiaChiCuaHang.requestFocus();
-                        edtDiaChiCuaHang.setError("Địa chỉ của cửa hàng từ 8-300 ký tự chữ số");
+                        edtDiaChiCuaHang.setError("Địa chỉ của cửa hàng từ 8-300 ký tự chữ và số");
                     }
                 }
                 return false;
@@ -87,7 +104,7 @@ public class DangKyActivity extends AppCompatActivity {
                     if(Validates.validEmail(edtEmailCuaHang.getText().toString()) == false) {
                         edtEmailCuaHang.selectAll();
                         edtEmailCuaHang.requestFocus();
-                        edtEmailCuaHang.setError("Email phải có định dạng xxxxx@gmail.com");
+                        edtEmailCuaHang.setError("1. Email phải có định dạng xxxxx@gmail.com \n 2. Email không được có 2 dấu chấm liền nhau");
                     }
                 }
                 return false;
@@ -108,14 +125,55 @@ public class DangKyActivity extends AppCompatActivity {
         });
 
 
-        //Chuyển sang màn hình đăng nhập
+
+        //Chọn hình CCCD Mặt trước
+        imgCCCDFront.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickImageFromGallery(PICK_IMAGE_FRONT);
+            }
+        });
+
+        //Chọn hình CCCD Mặt sau
+        imgCCCDBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickImageFromGallery(PICK_IMAGE_BACK);
+            }
+        });
+
+
         tvDaCoTaiKhoan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent intent = new Intent(DangNhapActivity.class);
-                setContentView(R.layout.activity_dang_nhap);
+                //Chuyển sang màn hình đăng nhập
+                Intent intent = new Intent(context, DangNhapActivity.class);
+                context.startActivity(intent);
             }
         });
+    }
+
+
+    //Hàm chọn ảnh từ thư viện
+    private void pickImageFromGallery(int requestCode) {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, requestCode);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PICK_IMAGE_FRONT) {
+                // Xử lý khi chọn ảnh cho mặt trước
+                Uri imageUri = data.getData();
+                imgCCCDFront.setImageURI(imageUri);
+            } else if (requestCode == PICK_IMAGE_BACK) {
+                // Xử lý khi chọn ảnh cho mặt sau
+                Uri imageUri = data.getData();
+                imgCCCDBack.setImageURI(imageUri);
+            }
+        }
     }
 
     private void setControl() {
@@ -128,7 +186,7 @@ public class DangKyActivity extends AppCompatActivity {
         imgCCCDFront = findViewById(R.id.imgCCCDFront);
         imgCCCDBack = findViewById(R.id.imgCCCDBack);
         chkDongYDieuKhoan = findViewById(R.id.chkDongYDieuKhoan);
-        btn_tiepTuc = findViewById(R.id.btn_tiepTuc);
+        btnTiepTuc = findViewById(R.id.btnTiepTuc);
         tvDaCoTaiKhoan = findViewById(R.id.tvDaCoTaiKhoan);
     }
 }
