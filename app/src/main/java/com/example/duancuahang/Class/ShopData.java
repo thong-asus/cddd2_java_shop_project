808b3f2e2818560f3e3ec7b56ab88e6ec372574b
thong-asus <truongthong.asustuf@gmail.com>
2023-10-30 16:41:57 +0700

    DONE REGISTRATION OTP_UPLOAD CCCD

--- app/src/main/java/com/example/duancuahang/Class/ShopData.java
+++ app/src/main/java/com/example/duancuahang/Class/ShopData.java
@@ -225,4 +225,22 @@ public class ShopData implements Serializable {
                 ", urlImgShopAvatar='" + urlImgShopAvatar + '\'' +
                 '}';
     }
+
+    @Override
+    public String toString() {
+        return "ShopData{" +
+                "status=" + status +
+                ", idShop='" + idShop + '\'' +
+                ", shopPhoneNumber='" + shopPhoneNumber + '\'' +
+                ", password='" + password + '\'' +
+                ", shopOwner='" + shopOwner + '\'' +
+                ", shopName='" + shopName + '\'' +
+                ", shopAddress='" + shopAddress + '\'' +
+                ", shopEmail='" + shopEmail + '\'' +
+                ", taxCode='" + taxCode + '\'' +
+                ", urlImgCCCDFront='" + urlImgCCCDFront + '\'' +
+                ", urlImgCCCDBack='" + urlImgCCCDBack + '\'' +
+                ", urlImgShopAvatar='" + urlImgShopAvatar + '\'' +
+                '}';
+    }
 }
